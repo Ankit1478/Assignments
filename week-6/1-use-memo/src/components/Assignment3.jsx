77f79bb01c216548b186027706b70c8beedd8f48
Ankit Raj <kumarankitraj1478@gmail.com>
2024-02-07 14:21:48 +0530

    useCallback assignment Done

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,18 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        {name:'Mango' , value:60}
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(()=>{
+        let sum = 0;
+        for(let i=0;i<items.length;i++){
+            sum+=items[i].value;
+        }
+        return sum;
+    },[items])
     // Your code ends here
     return (
         <div>
